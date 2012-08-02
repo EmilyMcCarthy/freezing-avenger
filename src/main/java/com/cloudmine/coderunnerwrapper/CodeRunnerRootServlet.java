55bafa3734502621ebef54c2674d69ffbf4da21a
JohnMcCarthy <john@cloudmine.me>
2012-08-02 14:42:41 -0400

    Updated for new argument types

--- src/main/java/com/cloudmine/coderunnerwrapper/CodeRunnerRootServlet.java
+++ src/main/java/com/cloudmine/coderunnerwrapper/CodeRunnerRootServlet.java
@@ -1,24 +1,26 @@
 package com.cloudmine.coderunnerwrapper;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import com.cloudmine.coderunner.SnippetArguments;
+import com.cloudmine.coderunner.SnippetContainer;
+import com.cloudmine.coderunner.SnippetResponseConfiguration;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.codehaus.jackson.map.ObjectMapper;
-import org.reflections.Reflections;
-
-import com.cloudmine.coderunner.SnippetContainer;
-import com.cloudmine.coderunner.SnippetResponseConfiguration;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class CodeRunnerRootServlet extends HttpServlet {
 	private static final long serialVersionUID = 771578936675722864L;
-
+    private static final Logger LOG = LoggerFactory.getLogger(CodeRunnerRootServlet.class);
 	private Map<String, SnippetContainer> snippetContainers;
 
 	@Override
@@ -30,35 +32,65 @@ public class CodeRunnerRootServlet extends HttpServlet {
 		}
 
 		Reflections reflections = new Reflections("com", "net", "org", "me", "io", "edu", "gov", "mil");
-		for (Class<? extends SnippetContainer> containerClass : reflections.getSubTypesOf(SnippetContainer.class)) {
+        Set<Class<? extends SnippetContainer>> subTypesOf = reflections.getSubTypesOf(SnippetContainer.class);
+        LOG.info("Found " + subTypesOf.size() + " subtypes");
+
+        for (Class<? extends SnippetContainer> containerClass : subTypesOf) {
 			try {
 				SnippetContainer container = containerClass.newInstance();
-				snippetContainers.put(container.getSnippetName(), container);
+                String snippetName = container.getSnippetName();
+                LOG.info("Storing snippetName: " + snippetName + " to: " + container);
+                snippetContainers.put(snippetName, container);
 			} catch (Exception e) {
-				e.printStackTrace();
+				LOG.error("Trouble putting in container", e);
 			}
 		}
 	}
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		String snippetName = req.getRequestURI().substring(1); // remove the first slash
-		@SuppressWarnings("unchecked") Map<String, String[]> parameterMap = req.getParameterMap(); // pass the parameter map along to the snippet
-		SnippetResponseConfiguration responseConfig = new SnippetResponseConfiguration();
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String snippetName = req.getRequestURI().substring(1); // remove the first slash
+        LOG.info("doPost called for snippetName: " + snippetName);
+        @SuppressWarnings("unchecked") Map<String, String[]> parameterMap = req.getParameterMap(); // pass the parameter map along to the snippet
 
-		// Check if the snippet container is available based on the path, and activate it if so.
-		// Otherwise render a 404 and stop.
-		if (snippetContainers.containsKey(snippetName)) {
-			SnippetContainer container = snippetContainers.get(snippetName);
-			Object snippetResponse = container.runSnippet(responseConfig, parameterMap);
+        SnippetResponseConfiguration responseConfig = new SnippetResponseConfiguration();
 
-			resp.setContentType(responseConfig.getMimeType());
+        // Check if the snippet container is available based on the path, and activate it if so.
+        // Otherwise render a 404 and stop.
+        if (snippetContainers.containsKey(snippetName)) {
+            LOG.info("Has key, calling snippet");
+            Map<String, String> convertedParamMap = new HashMap<String, String>();
+            for(String key : parameterMap.keySet()) {
+                String[] values = parameterMap.get(key);
+                String valueAsString = getValueAsString(values);
+                LOG.info(key + ":" + valueAsString);
+                convertedParamMap.put(key, valueAsString);
+            }
+            SnippetContainer container = snippetContainers.get(snippetName);
+            Object snippetResponse = container.runSnippet(new SnippetArguments(responseConfig, convertedParamMap));
+            resp.setContentType(responseConfig.getMimeType());
 
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.writeValue(resp.getOutputStream(), snippetResponse);
-		} else {
-			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-		}
-		resp.flushBuffer();
-	}
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getOutputStream(), snippetResponse);
+        } else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+        resp.flushBuffer();
+    }
+
+    private String getValueAsString(String[] values) {
+        switch(values.length) {
+            case 0:
+                return "";
+            case 1:
+                return values[0];
+            default:
+                LOG.error("String array received; should only receive one String per parameter. Concatenating");
+                StringBuilder builder = new StringBuilder();
+                for(int i = 0; i < values.length; i++) {
+                    builder.append(values[i]);
+                }
+                return builder.toString();
+        }
+    }
 }
