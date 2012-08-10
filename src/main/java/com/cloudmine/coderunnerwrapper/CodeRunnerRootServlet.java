5e7d6c7cc1d07b296fbde478909fd99e46bc6bb3
JohnMcCarthy <john@cloudmine.me>
2012-08-10 17:05:51 -0400

    Update for async code running

--- src/main/java/com/cloudmine/coderunnerwrapper/CodeRunnerRootServlet.java
+++ src/main/java/com/cloudmine/coderunnerwrapper/CodeRunnerRootServlet.java
@@ -3,7 +3,6 @@ package com.cloudmine.coderunnerwrapper;
 import com.cloudmine.coderunner.SnippetArguments;
 import com.cloudmine.coderunner.SnippetContainer;
 import com.cloudmine.coderunner.SnippetResponseConfiguration;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,31 +50,48 @@ public class CodeRunnerRootServlet extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String snippetName = req.getRequestURI().substring(1); // remove the first slash
         LOG.info("doPost called for snippetName: " + snippetName);
+
         @SuppressWarnings("unchecked") Map<String, String[]> parameterMap = req.getParameterMap(); // pass the parameter map along to the snippet
 
-        SnippetResponseConfiguration responseConfig = new SnippetResponseConfiguration();
+
 
         // Check if the snippet container is available based on the path, and activate it if so.
         // Otherwise render a 404 and stop.
         if (snippetContainers.containsKey(snippetName)) {
             LOG.info("Has key, calling snippet");
-            Map<String, String> convertedParamMap = new HashMap<String, String>();
-            for(String key : parameterMap.keySet()) {
-                String[] values = parameterMap.get(key);
-                String valueAsString = getValueAsString(values);
-                LOG.info(key + ":" + valueAsString);
-                convertedParamMap.put(key, valueAsString);
-            }
+            Map<String, String> convertedParamMap = convertParameterMap(parameterMap);
             SnippetContainer container = snippetContainers.get(snippetName);
-            Object snippetResponse = container.runSnippet(new SnippetArguments(responseConfig, convertedParamMap));
-            resp.setContentType(responseConfig.getMimeType());
 
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(resp.getOutputStream(), snippetResponse);
+            String asyncString = convertedParamMap.get("async");
+            boolean isAsync = asyncString != null && Boolean.parseBoolean(asyncString);
+            long startTime = System.currentTimeMillis();
+            if(isAsync) {
+                LOG.info("Running asynchronously");
+                RunnableSnippet snippet = new RunnableSnippet(container, new SnippetArguments(new SnippetResponseConfiguration(), convertedParamMap));
+                new Thread(snippet).start();
+            } else {
+                LOG.info("Running synchronously, a response will be returned");
+                RunnableSnippet snippet = new RunnableSnippet(container, new SnippetArguments(new SnippetResponseConfiguration(), convertedParamMap), resp);
+                snippet.run();
+            }
+            long totalTime = System.currentTimeMillis() - startTime;
+            LOG.info("Ran for: " + totalTime + "ms");
         } else {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            resp.flushBuffer();
         }
-        resp.flushBuffer();
+
+    }
+
+    private Map<String, String> convertParameterMap(Map<String, String[]> parameterMap) {
+        Map<String, String> convertedParamMap = new HashMap<String, String>();
+        for(String key : parameterMap.keySet()) {
+            String[] values = parameterMap.get(key);
+            String valueAsString = getValueAsString(values);
+            LOG.info(key + ":" + valueAsString);
+            convertedParamMap.put(key, valueAsString);
+        }
+        return convertedParamMap;
     }
 
     private String getValueAsString(String[] values) {
